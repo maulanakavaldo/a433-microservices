461f9bd83fc84c0a6be0a00ad3112f189a6dc485
maulanakavaldo <alkav.maulana@gmail.com>
2024-10-02 21:21:48 +0700

    Add build_push_image_karsajobs.sh

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -4,8 +4,8 @@
 docker build -t ghcr.io/maulanakavaldo/karsajobs:latest .
 
 # Log in to GitHub Container Registry
-# docker login ghcr.io -u maulanakavaldo -p $VAR-ENV
-echo $PAT | docker login ghcr.io --username maulanakavaldo --password-stdin
+# docker login ghcr.io -u maulanakavaldo -p $PACKAGE_TOKEN
+# echo $PAT | docker login ghcr.io --username maulanakavaldo --password-stdin
 
 # Push Docker image to GitHub Container Registry
 docker push ghcr.io/maulanakavaldo/karsajobs:latest
